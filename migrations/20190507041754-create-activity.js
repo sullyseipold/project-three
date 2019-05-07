15f8e0b6de5f9f844fcb167be66d337c0f567718
Kelly davis <kellyadavis7@gmail.com>
2019-05-07 16:01:43 -0400

    migrations

---
+++ migrations/20190507041754-create-activity.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Activities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      starttime: {
+        type: Sequelize.DATE
+      },
+      endtime: {
+        type: Sequelize.DATE
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Activities');
+  }
+};
